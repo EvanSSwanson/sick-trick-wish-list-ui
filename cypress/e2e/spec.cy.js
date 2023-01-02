1bb03c5185009a2bd92ddf9ed1bfbe3a39ab42aa
Evan Swanson <evan.sundelius@gmail.com>
2023-01-02 15:55:00 -0600

    Begin testing

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,15 @@
-describe('empty spec', () => {
+describe('App test', () => {
+  beforeEach(() => {
+    cy.intercept("http://localhost:3001/api/v1/tricks", {
+      method: 'GET',
+      fixture: '../fixtures/tricks.json'
+    });
+    cy.visit('http://localhost:3000/')
+  })
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:3000/')
+  })
+  it('Should have a title', () => {
+    cy.contains('h1', 'Sick Trick Wish List')
   })
 })
